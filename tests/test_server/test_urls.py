55d3c8a65f177dd87afd0fc0152fb740cdb9ebde
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2023-12-05 10:48:55 +0300

    test: Implement two tast cases for chacking the admin panel

---
+++ tests/test_server/test_urls.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from django.test import Client
+import pytest
+
+
+def test_admin_unauthorized(client: Client) -> None:
+    '''This test ensures that admin panel requires auth.'''
+    response = client.get('/admin/')
+    assert response.status_code == HTTPStatus.FOUND
+
+
+def test_admin_authorized(admin_client: Client) -> None:
+    '''This test ensures that admin panel is accessible.'''
+    response = admin_client.get('/admin/')
+    assert response.status_code == HTTPStatus.OK
