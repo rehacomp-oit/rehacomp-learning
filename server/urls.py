e16aff1cdcc43289398fc2814117b4c2eac4d6aa
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2023-12-10 15:58:49 +0300

    feat(templates): Implement serving static txt files with metadata

--- server/urls.py
+++ server/urls.py
@@ -6,8 +6,23 @@ Include other URLConfs from external apps using method `include()`.
 
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
+
+
+# Serving text and xml static files:
+__robots_view = TemplateView.as_view(
+    template_name='txt/robots.txt',
+    content_type='text/plain',
+)
+
+__humans_view = TemplateView.as_view(
+    template_name='txt/humans.txt',
+    content_type='text/plain',
+)
 
 
 urlpatterns = (
     path('admin/', admin.site.urls),
+    path('robots.txt', __robots_view),
+    path('humans.txt', __humans_view),
 )
--- tests/test_server/test_urls.py
+++ tests/test_server/test_urls.py
@@ -14,3 +14,14 @@ def test_admin_authorized(admin_client: Client) -> None:
     '''This test ensures that admin panel is accessible.'''
     response = admin_client.get('/admin/')
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize('page', (
+    '/robots.txt',
+    '/humans.txt',
+))
+def test_specials_txt(client: Client, page: str) -> None:
+    '''This test ensures that special `txt` files are accessible.'''
+    response = client.get(page)
+    assert response.status_code == HTTPStatus.OK
+    assert response.get('Content-Type') == 'text/plain'
