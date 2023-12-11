4e536ce002025606f809a5a57972d9777c595505
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2023-12-11 17:33:04 +0300

    feat: Implement postgresql support using docker
    now, to create a file with environment variables, you need to
    use the template in the <config> directory.
    The CI pipeline has been rewritten to use docker

---
+++ scripts/create_dev_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database locally.
+
+Note: ONLY development database;
+*/
+
+CREATE USER "rehacomp_learning" SUPERUSER;
+CREATE DATABASE "rehacomp_learning" OWNER "rehacomp_learning" ENCODING 'utf-8';
--- server/settings/components/database.py
+++ server/settings/components/database.py
@@ -1,6 +1,7 @@
 '''Database configuration for the project'''
 
-from server.settings.components import BASE_DIR
+from server.settings.components import env_config
+
 
 
 # Default primary key field type
@@ -8,7 +9,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.joinpath('db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env_config('POSTGRES_DB'),
+        'USER': env_config('POSTGRES_USER'),
+        'PASSWORD': env_config('POSTGRES_PASSWORD'),
+        'HOST': env_config('DJANGO_DATABASE_HOST'),
+        'PORT': env_config('DJANGO_DATABASE_PORT', default=5432, cast=int),
+        'CONN_MAX_AGE': env_config('CONN_MAX_AGE', cast=int, default=60),
+        'OPTIONS': {
+            'connect_timeout': 10,
+            'options': '-c statement_timeout=15000ms',
+        },
     },
 }
