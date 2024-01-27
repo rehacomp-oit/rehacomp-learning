f5f8cfb7d77d7688d13720d90427bca7ea63d6a7
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-01-27 06:59:06 +0300

    refactor: Combine all url pattern tests
    All tests checking the correctness of the
    url patterns have been moved to
    a separate module.
    This solved the problem of
    code duplication.

---
+++ tests/test_url_patterns.py
@@ -0,0 +1,53 @@
+'''
+Test cases for all url patterns in this application
+'''
+
+from collections import namedtuple
+from typing import Final
+
+from django.urls import resolve
+import pytest
+
+
+# Datastructure for storing the related information with url address
+UrlMetadata = namedtuple(
+    'UrlMetadata',
+    ('url_name', 'view_name',),
+)
+
+
+URL_PATTERN_FIXTURE: Final = {
+    '/': UrlMetadata('index', 'index'),
+    '/main/profile/': UrlMetadata('profile', 'main:profile'),
+    '/accounts/login/': UrlMetadata('login', 'accounts:login'),
+    '/accounts/register/': UrlMetadata('register', 'accounts:register'),
+}
+
+
+@pytest.mark.parametrize('route', URL_PATTERN_FIXTURE.keys())
+def test_correct_url_name(route: str) -> None:
+    '''this test ensures that the url is mapped to the correct url name'''
+
+    expected_url_name = URL_PATTERN_FIXTURE[route].url_name
+    resolver = resolve(route)
+    assert resolver.url_name == expected_url_name
+
+
+@pytest.mark.parametrize('route', URL_PATTERN_FIXTURE.keys())
+def test_correct_view_name(route: str) -> None:
+    '''this test ensures that the url is mapped to the correct view name'''
+
+    expected_view_name = URL_PATTERN_FIXTURE[route].view_name
+    resolver = resolve(route)
+    assert resolver.view_name == expected_view_name
+
+
+def test_resolve_index() -> None:
+    '''
+    This test ensures that the profile url and the root url
+    refer to the same view function
+    '''
+
+    profile_url_resolver = resolve('/main/profile/')
+    index_url_resolver = resolve('/')
+    assert index_url_resolver.func is profile_url_resolver.func
