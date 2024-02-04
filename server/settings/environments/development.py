c41e34b955fe599c0eed33e4118e24b4d7e19b5a
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-02-04 13:42:12 +0300

    chore: Change the development environment configuration

--- server/settings/environments/development.py
+++ server/settings/environments/development.py
@@ -1,4 +1,18 @@
 '''This file contains all the settings that defines the development server.'''
 
+from server.settings.components.database import DATABASES
+
+
+# Setting the development status:
 DEBUG = True
-ALLOWED_HOSTS: tuple[str, ...] = ()
+
+ALLOWED_HOSTS = (
+    'localhost',
+    '0.0.0.0',
+    '127.0.0.1',
+    '[::1]',
+)
+
+# Disable persistent DB connections
+# https://docs.djangoproject.com/en/4.2/ref/databases/#caveats
+DATABASES['default']['CONN_MAX_AGE'] = 0
