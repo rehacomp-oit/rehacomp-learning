e12f352b9cff1ddf3c37853abe35a615068479c7
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-02-06 14:40:37 +0300

    refactor: Change the view for user registration
    - defined the constants for template name, URLs, etc;
    - processing of the post method has been moved to a separate function.

--- server/apps/accounts/views.py
+++ server/apps/accounts/views.py
@@ -1,3 +1,5 @@
+from typing import Final
+
 from django.contrib.auth.views import LoginView
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
@@ -6,18 +8,32 @@ from django.urls import reverse_lazy
 from .forms import RegisterForm
 
 
+LOGIN_SUCCESS_URL_NAME: Final = 'main:profile'
+REGISTER_SUCCESS_URL_NAME: Final = 'accounts:login'
+REGISTRATION_PAGE_TEMPLATE_NAME: Final = 'accounts/register.html'
+LOGIN_PAGE_TEMPLATE_NAME: Final = 'accounts/login.html'
+
+
 login_user = LoginView.as_view(
-    template_name='accounts/login.html',
-    next_page=reverse_lazy('main:profile'),
+    template_name=LOGIN_PAGE_TEMPLATE_NAME,
+    next_page=reverse_lazy(LOGIN_SUCCESS_URL_NAME),
 )
+
+
+def __try_register_user(
+    request: HttpRequest
+) -> HttpResponse | HttpResponseRedirect:
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect(REGISTER_SUCCESS_URL_NAME)
+
+    return render(request, REGISTRATION_PAGE_TEMPLATE_NAME, {'form': form})
 
 
 def register_user(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
+        return __try_register_user(request)
 
     form = RegisterForm()
-    return render(request, 'accounts/register.html', {'form': form})
+    return render(request, REGISTRATION_PAGE_TEMPLATE_NAME, {'form': form})
