1a898af40587f0ebc5d10e3933f01d79f2069693
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-02-28 14:38:02 +0300

    feat: Create a custom login view

--- server/apps/accounts/views/login_user.py
+++ server/apps/accounts/views/login_user.py
@@ -1,14 +1,37 @@
 from typing import Final
 
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
+from django.contrib.messages import error
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from server.apps.accounts.forms import LoginForm
 
 
 LOGIN_SUCCESS_URL_NAME: Final = 'main:profile'
 LOGIN_PAGE_TEMPLATE_NAME: Final = 'accounts/login.html'
 
 
-login_user = LoginView.as_view(
-    template_name=LOGIN_PAGE_TEMPLATE_NAME,
-    next_page=reverse_lazy(LOGIN_SUCCESS_URL_NAME),
-)
+def __handle_get_request(request: HttpRequest) -> HttpResponse:
+    form = LoginForm()
+    return render(request, LOGIN_PAGE_TEMPLATE_NAME, {'form': form})
+
+
+def __handle_post_request(
+    request: HttpRequest
+) -> HttpResponse | HttpResponseRedirect:
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        user = authenticate(request, **form.cleaned_data)
+        if user is not None:
+            login(request, user)
+            return redirect(LOGIN_SUCCESS_URL_NAME)
+
+    error(request, 'Не удалось войти в систему')
+    return render(request, LOGIN_PAGE_TEMPLATE_NAME, {'form': form})
+
+
+def login_user(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+    if request.method == 'POST':
+        return __handle_post_request(request)
+
+    return __handle_get_request(request)
