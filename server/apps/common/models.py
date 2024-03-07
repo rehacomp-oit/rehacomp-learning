b31574cb20a6612be356f67bab7dc99d050024f1
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-03-07 14:23:54 +0300

    feat: Implement a model to represent training courses

--- server/apps/common/models.py
+++ server/apps/common/models.py
@@ -24,3 +24,33 @@ class VOSOrganization(Model):
 
     def __str__(self) -> str:
         return self.organization_name
+
+
+@final
+class Course(Model):
+    '''Training course conducted at the Institute'''
+
+    class Meta:
+        db_table = 'courses'
+        db_table_comment = 'Training course conducted at the Institute'
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+    course_name = CharField(
+        max_length=80,
+        db_comment='Full name of the training course',
+        verbose_name='Полное название курса',
+        unique=True,
+    )
+
+    course_short_name = CharField(
+        max_length=10,
+        db_comment='Abbreviation of the course name',
+        verbose_name='Аббревиатура названия курса',
+        unique=True,
+    )
+
+
+    def __str__(self) -> str:
+        return self.course_short_name
--- tests/test_apps/test_common/test_migrations.py
+++ tests/test_apps/test_common/test_migrations.py
@@ -14,3 +14,22 @@ def test_0001_vos_organization_model(migrator: Migrator) -> None:
     )
     model = new_state.apps.get_model('common', 'VOSOrganization')
     assert model.objects.create(organization_name='some organization')
+
+
+def test_0002_course_model(migrator: Migrator) -> None:
+    '''
+    Tests a migration that creates a table
+    for storing information about courses.
+    '''
+
+    old_state = migrator.apply_initial_migration(
+        ('common', '0001_vos_organization_model',)
+    )
+    with raises(LookupError):
+        old_state.apps.get_model('common', 'Course')
+
+    new_state = migrator.apply_tested_migration(
+        ('common', '0002_course_model')
+    )
+    model = new_state.apps.get_model('common', 'Course')
+    assert model.objects.create(course_name='s1', course_short_name='s2')
