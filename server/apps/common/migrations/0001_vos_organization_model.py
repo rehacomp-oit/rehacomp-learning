7f8867fcab322962d3799528e73b5d31465fcca2
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-03-10 15:31:31 +0300

    refactor: Improve the readability of the code of the first application migration
    puted the description of fields and
    options outside the migration class

--- server/apps/common/migrations/0001_vos_organization_model.py
+++ server/apps/common/migrations/0001_vos_organization_model.py
@@ -3,35 +3,38 @@ from django.db.migrations import Migration as BaseMigration
 from django.db.models import BigAutoField, CharField
 
 
+_model_fields = (
+    ('id', BigAutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID',
+    )),
+    ('organization_name', CharField(
+        db_comment='Full name of the organization',
+        max_length=80,
+        verbose_name='Наиминование организации ВОС',
+        unique=True,
+    )),
+)
+
+_model_options = {
+    'verbose_name': 'организация ВОС',
+    'verbose_name_plural': 'организации ВОС',
+    'db_table': 'vos_organizations',
+    'db_table_comment': 'All Russia Association of the Blind',
+}
+
+
 class Migration(BaseMigration):
 
     initial = True
     dependencies = ()
 
-    __model_fields = (
-        ('id', BigAutoField(
-            auto_created=True,
-            primary_key=True,
-            serialize=False,
-            verbose_name='ID',
-        )),
-        ('organization_name', CharField(
-            db_comment='Full name of the organization',
-            max_length=80,
-            verbose_name='Наиминование организации ВОС',
-            unique=True,
-        )),
-    )
-
     operations = (
         CreateModel(
             name='VOSOrganization',
-            fields=__model_fields,
-            options={
-                'verbose_name': 'организация ВОС',
-                'verbose_name_plural': 'организации ВОС',
-                'db_table': 'vos_organizations',
-                'db_table_comment': 'All Russia Association of the Blind',
-            },
+            fields=_model_fields,
+            options=_model_options,
         ),
     )
