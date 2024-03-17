a5b292605d9a0d2d623e06b1dfb66e0fb9409847
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-03-17 17:44:26 +0300

    feat: Exclude type extension for django from dependencies
    in this moment <django-stubbs>
    does not support 5.x django version

--- server/settings/__init__.py
+++ server/settings/__init__.py
@@ -11,13 +11,8 @@ To change settings file:
 
 from os import environ
 
-import django_stubs_ext
 from split_settings.tools import include
 
-
-# Monkeypatching Django, so stubs will work for all generics,
-# see: https://github.com/typeddjango/django-stubs
-django_stubs_ext.monkeypatch()
 
 # Managing environment via `DJANGO_ENV` variable:
 environ.setdefault('DJANGO_ENV', 'development')
