e80b538060183f345fe44b0a64eb7d5fdb608d89
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-03-28 16:53:53 +0300

    refactor: Remove redundant magic constants from the model definition

--- server/apps/common/constants.py
+++ server/apps/common/constants.py
@@ -1,11 +1,26 @@
+'''
+A set of constants used in defining models
+as well as in related business scenarios.
+'''
+
 from typing import Final
 from typing import final
 
 from django.db.models import IntegerChoices, TextChoices
 
 
+VOS_ORGANIZATION_NAME_LENGTH: Final = 80
+COURSE_FULL_NAME_LENGTH: Final = 80
+COURSE_SHORT_NAME_LANGTH: Final = 10
 EMAIL_ADDRESS_PATTERN: Final = r'^\S+@\S+\.\S+$'
 PHONE_NUMBER_PATTERN: Final = r'^\+7\(\d{3}\)\d{3}\-\d{2}\-\d{2}$'
+PHONE_NUMBER_MAX_LENGTH: Final = 20
+# A person's first name, patronymic and last name
+# have the same number of characters.
+FULL_NAME_LENGTH: Final = 20
+DISABILITY_GROUP_LENGTH: Final = 5
+EDUCATION_INFORMATION_LENGTH: Final = 80
+JOB_INFORMATION_LENGTH: Final = 80
 
 
 @final
