f4b6ccbf3367fc06727e63ef43bac44f17aaa0d1
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-06-13 10:07:31 +0300

    feat: Add a http method verification for profile view function

--- server/apps/learning/views.py
+++ server/apps/learning/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 
 # fake view because this do nothing.
 @login_required
+@require_http_methods(('GET',))
 def index(request: HttpRequest) -> HttpResponse:
     '''
 Main (or index) view.
