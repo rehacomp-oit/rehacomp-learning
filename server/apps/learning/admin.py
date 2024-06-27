f7d5e3969c5f9c6536d0324d7dfc19eea4df89e2
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-06-27 16:31:44 +0300

    feat: Add a model describing the VOS organization to the admin panel

--- server/apps/learning/admin.py
+++ server/apps/learning/admin.py
@@ -1,1 +1,11 @@
-# Future admin panel
+from django.contrib.admin import ModelAdmin, ShowFacets, site
+
+
+from .models import VOSOrganization
+
+
+class ExtendedModelAdmin(ModelAdmin):
+    show_facets = ShowFacets.NEVER
+
+
+site.register(VOSOrganization, ExtendedModelAdmin)
