4b8a921ce5ca8344c31477c1b317bcfcfd01eb04
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-06-30 14:58:11 +0300

    feat: Add corrected tests for the <Course> model

--- tests/test_apps/test_learning/test_migrations.py
+++ tests/test_apps/test_learning/test_migrations.py
@@ -20,3 +20,21 @@ def test_0001_vos_organization_model(migrator: Migrator) -> None:
     new_state = migrator.apply_tested_migration((APPLICATION_NAME, '0001_vos_organization_model'))
     model = new_state.apps.get_model(APPLICATION_NAME, MODEL_NAME)
     assert model.objects.create(organization_name='some organization')
+
+
+@mark.django_db
+def test_0002_course_model(migrator: Migrator) -> None:
+    '''
+    Tests a migration that creates a table
+    for storing information about courses.
+    '''
+
+    MODEL_NAME: Final = 'Course'
+
+    old_state = migrator.apply_initial_migration((APPLICATION_NAME, '0001_vos_organization_model',))
+    with raises(LookupError):
+        old_state.apps.get_model(APPLICATION_NAME, MODEL_NAME)
+
+    new_state = migrator.apply_tested_migration((APPLICATION_NAME, '0002_course_model'))
+    model = new_state.apps.get_model(APPLICATION_NAME, MODEL_NAME)
+    assert model.objects.create(course_name='s1', course_short_name='s2')
