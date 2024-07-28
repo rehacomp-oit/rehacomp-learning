72a5867e618d387100aafa2b42b80777acd03012
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-07-28 05:57:36 +0300

    fix: Rewrite migrations for the <core> application

---
+++ server/apps/core/migrations/0001_vos_organization_model.py
@@ -0,0 +1,34 @@
+from typing import final
+
+from django.db.migrations import CreateModel
+from django.db.migrations import Migration as BaseMigration
+from django.db.models.fields import CharField, SmallAutoField
+
+
+_fields = (
+    ('id', SmallAutoField(
+        auto_created=True,
+        primary_key=True,
+        serialize=False,
+        verbose_name='ID'
+    )),
+
+    ('organization_name', CharField(
+        max_length=80,
+        unique=True,
+        verbose_name='VOS organization name'
+    )),
+)
+
+_options = {
+    'verbose_name': 'VOS organization',
+    'verbose_name_plural': 'VOS organizations',
+    'db_table_comment': 'All Russia Association of the Blind.',
+}
+
+
+@final
+class Migration(BaseMigration):
+    initial = True
+    dependencies = ()
+    operations = (CreateModel(name='VOSOrganization', fields=_fields, options=_options),)
---
+++ server/apps/core/migrations/0005_related_organization_for_person.py
@@ -0,0 +1,34 @@
+from typing import final
+
+from django.db.migrations import AddField, RunSQL
+from django.db.migrations import Migration as BaseMigration
+from django.db.models import ForeignKey
+from django.db.models.deletion import SET_NULL
+
+
+_field_definition = ForeignKey(
+    db_constraint=False,
+    null=True,
+    on_delete=SET_NULL,
+    related_name='members',
+    to='core.vosorganization',
+    verbose_name='Related VOS organization'
+)
+
+_foreign_key_constraint_definition = (
+    '-- Create constraint related_vos_fk on model person\n'
+    'ALTER TABLE "core_person" ADD CONSTRAINT "related_vos_fk" '
+    'FOREIGN KEY ("related_vos_organization_id") REFERENCES "core_vosorganization" ("id") '
+    'ON DELETE SET NULL '
+    'DEFERRABLE INITIALLY DEFERRED;'
+)
+
+
+@final
+class Migration(BaseMigration):
+    dependencies = (('core', '0004_checks_for_person_model'),)
+
+    operations = (
+        AddField(model_name='person', name='related_vos_organization', field=_field_definition),
+        RunSQL(_foreign_key_constraint_definition),
+    )
