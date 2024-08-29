18d9cf3640df09440da7713a03a90f27c3c3dfe3
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-08-29 18:52:20 +0300

    feat: implement a function for transliterating text in Russian

--- server/common/utils.py
+++ server/common/utils.py
@@ -3,3 +3,14 @@ from ulid import new, ULID
 
 def make_ULID() -> ULID:
     return new()
+
+
+def transliterate_text(text: str) -> str:
+    '''
+    transliterates the letters of the Russian alphabet.
+    '''
+
+    source = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+    destenation = 'abvgdeejzijklmnoprstufhzcss_y_eua'
+    translate_table = str.maketrans(source, destenation)
+    return text.lower().translate(translate_table)
