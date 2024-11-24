f6481658fdf7be7dfb42b5bed2b8f3c1f6cabe90
Evgeniy Tulyakov <e.v.tulyakov1998@gmail.com>
2024-11-25 02:58:45 +0300

    feat: implement a decorator for error control in the infrastructure layer
    It is important to use this decorator for methods or functions
    that may raise exceptions that require handling to ensure
    the reliability and stability of this application's infrastructure layer.

--- server/common/infrastructure.py
+++ server/common/infrastructure.py
@@ -2,9 +2,19 @@
 Implementation of a common infrastructure layer.
 '''
 
+from collections.abc import Callable
+from functools import wraps
+from typing import ParamSpec, TypeVar
+
 from ulid import new as generate_ulid
 
 from .domain import EntityId
+from .exceptions import InfrastructureLayerError
+
+
+# Annotation helpers for decorators definition:
+F_Spec = ParamSpec('F_Spec')
+F_Return = TypeVar('F_Return')
 
 
 def get_ulid_for_pk() -> EntityId:
@@ -13,3 +23,31 @@ def get_ulid_for_pk() -> EntityId:
     '''
 
     return EntityId(generate_ulid())
+
+
+def make_safe(
+    expected_exception: type[Exception] | None=None
+) -> Callable[[Callable[F_Spec, F_Return]], Callable[F_Spec, F_Return]]:
+    '''
+    Wraps functions or methods of the infrastructure layer, ensuring error handling.
+
+    If a specified exception is raised, the decorator will raise an <InfrastructureLayerError> exception.
+    If no exception is passed to the decorator, the base Exception class will be caught.
+
+    :param expected_exception: The specific exception type to catch during the function's execution.
+    If not provided, it will default to capturing all exceptions derived from the base Exception.
+
+    :raises InfrastructureLayerError: if specified or general exception occurs.
+    '''
+
+    def _decorate(operation: Callable[F_Spec, F_Return]) -> Callable[F_Spec, F_Return]:
+        @wraps(operation)
+        def _wrapper(*args: F_Spec.args, **kwargs: F_Spec.kwargs) -> F_Return:
+            ExceptionClass = expected_exception or Exception
+            try:
+                return operation(*args, **kwargs)
+            except ExceptionClass as catched:
+                raise InfrastructureLayerError from catched
+
+        return _wrapper
+    return _decorate
